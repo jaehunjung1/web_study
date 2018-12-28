76a20f60feafde2c3451cd2b914c5611112fc196
sharkmir1 <sharkmir1@snu.ac.kr>
2018-12-29 00:07:36 +0900

    templates

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateField()
+    body = models.CharField(max_length=480)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+    
